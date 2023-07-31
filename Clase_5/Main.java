fd6492d181749ba4016473668966531379d48f56
Markosgt7 <markos.guate7@gmail.com>
2023-07-31 11:01:32 -0600

    Ejercicio mayor y menor de edad

--- Clase_5/Main.java
+++ Clase_5/Main.java
@@ -1,7 +1,16 @@
 package Clase_5;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hola mundo");
+       Scanner cargarDatos = new Scanner(System.in);
+       String nombre = "Marcos";
+       int edad = cargarDatos.nextInt();
+       if(edad >= 18){
+           System.out.println(nombre + "Eres mayor de edad");
+       }else{
+           System.out.println(nombre + "Eres menor de edad");
+       }
+
     }
 }
