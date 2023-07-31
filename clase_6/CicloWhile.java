738722ed566b30c7fe4f0e1e547f2dcad86a1a2e
Markosgt7 <markos.guate7@gmail.com>
2023-07-31 15:02:26 -0600

    Ejempo de ciclo while

---
+++ clase_6/CicloWhile.java
@@ -0,0 +1,11 @@
+package clase_6;
+
+public class CicloWhile {
+    public static void main(String[] args){
+        int contador = 0;
+        while (contador < 3){
+            contador++;
+            System.out.println("Contador:"+ contador);
+        }
+    }
+}
