afb25c3afb8dcd589a9c7426515e64b04714b9ac
Markosgt7 <markos.guate7@gmail.com>
2023-07-31 15:26:06 -0600

    Ejemplo de un arreglo(array) en java

---
+++ Clase_7/Arreglos.java
@@ -0,0 +1,20 @@
+package Clase_7;
+
+public class Arreglos {
+    public static void main(String[] args){
+        String[] ciudades = new String[10];
+        ciudades[0] = "Medellín";
+        ciudades[1] = "Bogotá";
+        ciudades[2] = "Guatemala";
+        ciudades[3] = "Paris";
+        ciudades[4] = "Madrid";
+        ciudades[5] = "Córdoba";
+        ciudades[6] = "Barcelona";
+        ciudades[7] = "Quito";
+        ciudades[8] = "Mexico DF";
+        ciudades[9] = "Antioquia";
+        String ciudadDeLosAlfajores = ciudades[5];
+        System.out.println("Ciudad de los alfajores->"+ ciudadDeLosAlfajores);
+
+    }
+}
